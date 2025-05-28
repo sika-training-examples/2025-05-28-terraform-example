f3421e171e67156b55197f828e54dbef50a15b32
Ondrej Sika <ondrej@ondrejsika.com>
2025-05-28 14:06:59 +0000

    feat: Add 2 more instances of app

--- apps.tf
+++ apps.tf
@@ -10,3 +10,30 @@ module "iceland_app" {
 output "iceland_app_url" {
   value = module.iceland_app.live_url
 }
+
+module "hello_world_app" {
+  source = "./modules/do_app"
+
+  for_each = {
+    en = {
+      text = "Hello World!"
+    }
+    cz = {
+      text = "Ahoj Svete!"
+    }
+  }
+
+  name                = each.key
+  image_registry_type = "GHCR"
+  image_registry      = "ghcr.io"
+  image_repository    = "sikalabs/hello-world-server"
+  instance_count      = 2
+  port                = 8000
+}
+
+output "hello_world_app_urls" {
+  value = {
+    for key, val in module.hello_world_app :
+    key => val.live_url
+  }
+}
