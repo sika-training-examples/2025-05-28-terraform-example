ad02debcfc8c47772dcc83365d9ede9409e840a9
Ondrej Sika <ondrej@ondrejsika.com>
2025-05-28 09:47:32 +0000

    feat: Add DB with sensitive output example

---
+++ postgres.tf
@@ -0,0 +1,13 @@
+resource "digitalocean_database_cluster" "db" {
+  name       = "example"
+  engine     = "pg"
+  version    = "17"
+  size       = "db-s-1vcpu-1gb"
+  region     = "fra1"
+  node_count = 1
+}
+
+output "db_conn" {
+  value     = digitalocean_database_cluster.db.uri
+  sensitive = true
+}
