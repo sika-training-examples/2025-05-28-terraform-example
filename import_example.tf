a5e3336c7c3cec4fb03ac1173d1b346a438668a9
Ondrej Sika <ondrej@ondrejsika.com>
2025-05-28 13:18:40 +0000

    feat: Add commented-out example of declarative import

---
+++ import_example.tf
@@ -0,0 +1,12 @@
+# import {
+#   to = digitalocean_droplet.demo
+#   id = "498899597"
+# }
+
+# resource "digitalocean_droplet" "demo" {
+#   backups = false
+#   image   = "debian-12-x64"
+#   name    = "demo"
+#   region  = "fra1"
+#   size    = "s-1vcpu-1gb-amd"
+# }
